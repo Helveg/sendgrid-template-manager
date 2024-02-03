932c1e837c9bf1fc92ade1763b3362778ff18932
Robin De Schepper <robin.deschepper93@gmail.com>
2024-02-03 09:52:39 +0100

    finish list assets command

---
+++ src/commands/list-assets.command.ts
@@ -0,0 +1,53 @@
+import { makeClient } from "../client";
+import {
+  filterContentTemplates,
+  listTemplates,
+} from "../templates/template.util";
+import { Client } from "@sendgrid/client";
+import { listDesigns } from "../designs/design.util";
+
+export async function listAssetsCommand(
+  type: "designs" | "templates" | undefined,
+  options: { key?: string },
+) {
+  const client = makeClient(options);
+  let out = "";
+  const promises = [];
+  if ((type ?? "templates") === "templates") {
+    promises.push(summarizeTemplates(client));
+  }
+  if ((type ?? "designs") === "designs") {
+    promises.push(summarizeDesigns(client));
+  }
+  console.log((await Promise.all(promises)).join("\n\n"));
+}
+
+async function summarizeTemplates(client: Client) {
+  let out = `Content templates\n-----------------\n\n`;
+  try {
+    const allTemplates = await listTemplates(client);
+    const contentTemplates = filterContentTemplates(allTemplates);
+    out += contentTemplates
+      .map((template) => `* ${template.name} [${template.id}]`)
+      .join("\n");
+    if (contentTemplates.length < allTemplates.length) {
+      out +=
+        (contentTemplates.length ? "\n\n" : "") +
+        `(${allTemplates.length - contentTemplates.length} untagged)`;
+    }
+  } catch (e) {
+    out += "  ERROR";
+  }
+  return out;
+}
+
+async function summarizeDesigns(client: Client) {
+  let out = `Designs\n-----------------\n\n`;
+  try {
+    const designs = await listDesigns(client);
+    out += designs.map((design) => `* ${design.name}`).join("\n");
+  } catch (e) {
+    out += "  ERROR";
+  }
+  return out;
+}
