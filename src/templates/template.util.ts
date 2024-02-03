23b4ac1c64206f861d83575aa69117e076a2e95c
Robin De Schepper <robin.deschepper93@gmail.com>
2024-02-03 09:54:10 +0100

    add template finding and updating

--- src/templates/template.util.ts
+++ src/templates/template.util.ts
@@ -1,9 +1,67 @@
-import { Template } from "./template.interfaces";
+import {
+  Template,
+  TemplateResponse,
+  TemplateVersion,
+  TemplateVersionResponse,
+} from "./template.interfaces";
+import { Client } from "@sendgrid/client";
+import { STMError } from "../errors";
+import { getAllModules, getModuleContainer, makeDom } from "../dom.util";
+
+export async function listTemplates(client: Client) {
+  const [response] = (await client.request({
+    url: `/v3/templates`,
+    method: "GET",
+    qs: {
+      generations: "dynamic",
+    },
+  })) as [TemplateResponse, any];
+  return response.body.templates;
+}
+
+export function findVersion(template: Template, name = "__content__") {
+  return template.versions.find((v) => v.name === name);
+}
+
+export async function retrieveContentVersion(
+  client: Client,
+  template: Template,
+) {
+  const version = findVersion(template);
+  if (!version) {
+    throw new STMError(
+      `Template '${template.name}' is missing a __content__ version.`,
+      { code: "template.no-content-version" },
+    );
+  }
+  const [response] = (await client.request({
+    url: `/v3/templates/${template.id}/versions/${version.id}`,
+    method: "GET",
+  })) as [TemplateVersionResponse, any];
+  return response.body;
+}
+
+export async function retrieveModules(client: Client, template: Template) {
+  const dom = makeDom(await retrieveContentVersion(client, template));
+  return getAllModules(getModuleContainer(dom));
+}
 
 export function filterContentTemplates(templates: Template[]) {
-  return templates.filter(
-    (template) =>
-      template.versions.filter((version: any) => version.name === "__content__")
-        .length,
-  );
+  return templates.filter((t) => findVersion(t));
+}
+
+export async function updateVersion(
+  client: Client,
+  template: Template,
+  version: Partial<TemplateVersion> & Pick<TemplateVersion, "id">,
+) {
+  const id = version.id;
+  const body: Partial<TemplateVersion> = { ...version };
+  delete body.id;
+  const [response] = (await client.request({
+    url: `/v3/templates/${template.id}/versions/${id}`,
+    body,
+    method: "PATCH",
+  })) as [TemplateVersionResponse, any];
+  return response.body;
 }
